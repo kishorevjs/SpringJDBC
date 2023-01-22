b0c7fa02db82487d9ec3144ea52dbc0e2d0f5be0
kishorevjs <jayasaikishore@gmail.com>
2023-01-22 15:40:44 -0800

    Pulling the database values using CommandLineRunner

---
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,21 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    //select * from person table and return all the content
+    public List<Person> findAll(){
+        return jdbcTemplate.query("select * from person",
+                new BeanPropertyRowMapper<Person>(Person.class));
+    }
+}
