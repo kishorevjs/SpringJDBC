dc936126118935bed753174ad1a2a3430d5fdc6b
kishorevjs <jayasaikishore@gmail.com>
2023-01-22 22:04:29 -0800

    Implementing findById JPA Repository Method

---
+++ src/main/java/com/in28minutes/database/databasedemo/SpringJPADemoApplication.java
@@ -0,0 +1,33 @@
+package com.in28minutes.database.databasedemo;
+
+import com.in28minutes.database.databasedemo.jpa.PersonJpaRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJPADemoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	PersonJpaRepo personJpaRepo;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJPADemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("User id 10007 is {}", personJpaRepo.findById(10007));
+		/*logger.info("All users --> {}", dao.findAll());
+		logger.info("User with the location {}", dao.findByLocation("USA"));
+		//logger.info("Users with date of birth {}", dao.findByBirthDate(java.sql.Date()));
+		logger.info("Id deleted 10007 - no.of rows deleted {}", dao.deleteById(10007));
+		logger.info("Inserted row is - {}",
+				dao.insert(new Person(10010, "Micheal", "Cuba", new Date())));
+		logger.info("Update id is 10003- {}",
+				dao.update(new Person(10003, "Vutukury", "India", new Date())));*/
+	}
+}
---
+++ src/main/java/com/in28minutes/database/databasedemo/jpa/PersonJpaRepo.java
@@ -0,0 +1,22 @@
+package com.in28minutes.database.databasedemo.jpa;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+//Repository
+//Transaction
+@Repository
+@Transactional
+public class PersonJpaRepo {
+
+    //connect to db
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Person findById(int id){
+        return entityManager.find(Person.class, id);
+    }
+}
